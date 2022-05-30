68111cbdf81f96e805c28604d01d6d81d2113867
Juan Da Silva <jcda_23@hotmail.com>
2022-05-30 00:08:03 -0500

    Se crea una nueva rama por problemas en el merge de la rama testing-4 con el master

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,3 @@
-/******** SCRIPT DE APERTURA Y CIERRE DE MENU MOBILE******* */
-((d) => {
-  const $btnMenu = d.querySelector(".menu-btn"),
-    $menu = d.querySelector(".navbar");
-
-  $btnMenu.addEventListener("click", (e) => {
-    $btnMenu.firstElementChild.classList.toggle("none");
-    $btnMenu.lastElementChild.classList.toggle("none");
-    $menu.classList.toggle("is-active");
-  });
-
-  d.addEventListener("click", (e) => {
-    if (!e.target.matches(".menu a")) return false;
-
-    $btnMenu.firstElementChild.classList.remove("none");
-    $btnMenu.lastElementChild.classList.add("none");
-    $menu.classList.remove("is-active");
-  });
-})(document);
-
-((d) => {
-  let video = d.querySelector(".video-guid");
-  window.addEventListener("scroll", function () {
-    let value = 1 + window.scrollY / -600;
-    video.style.opacity = value;
-  });
-})(document);
-
 var swiper = new Swiper(".swiper-container", {
   navigation: {
     nextEl: ".swiper-button-next",
@@ -75,3 +47,23 @@ var swiper = new Swiper(".swiper-container", {
     },
   },
 });
+
+/******** SCRIPT DE APERTURA Y CIERRE DE MENU MOBILE******* */
+((d) => {
+  const $btnMenu = d.querySelector(".menu-btn"),
+    $menu = d.querySelector(".navbar");
+
+  $btnMenu.addEventListener("click", (e) => {
+    $btnMenu.firstElementChild.classList.toggle("none");
+    $btnMenu.lastElementChild.classList.toggle("none");
+    $menu.classList.toggle("is-active");
+  });
+
+  d.addEventListener("click", (e) => {
+    if (!e.target.matches(".menu a")) return false;
+
+    $btnMenu.firstElementChild.classList.remove("none");
+    $btnMenu.lastElementChild.classList.add("none");
+    $menu.classList.remove("is-active");
+  });
+})(document);
